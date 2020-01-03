56987e45c9264d1b87ec87f83b03f6ffeb3d0d0a
vbu_apptech <vbu@apptech-team.ru>
2020-01-03 16:50:10 +0300

    fix screen orientation and colors for filled state custom edit text

--- app/src/main/java/com/example/arenamsk/custom_view/AbstractCustomEditText.kt
+++ app/src/main/java/com/example/arenamsk/custom_view/AbstractCustomEditText.kt
@@ -40,7 +40,9 @@ abstract class AbstractCustomEditText: LinearLayout {
         hint = custom_text_layout.hint.toString()
 
         custom_edit_text.setOnFocusChangeListener { _, hasFocus ->
-            currentState = if (!hasFocus && custom_edit_text.text.toString().isEmpty()) {
+            currentState = if (hasFocus) {
+                State.FILLED
+            } else if (!hasFocus && custom_edit_text.text.toString().isEmpty()) {
                 State.EMPTY
             } else {
                 State.FILLED
@@ -67,7 +69,7 @@ abstract class AbstractCustomEditText: LinearLayout {
                 custom_edit_text.background = resources.getDrawable(R.drawable.edit_text_filled_background)
                 custom_text_layout.hint = hint
                 custom_text_layout.defaultHintTextColor = ColorStateList.valueOf(resources.getColor(
-                    R.color.edit_text_hint_color))
+                    R.color.edit_text_highlight_hint_color))
                 custom_text_layout.hintTextColor = ColorStateList.valueOf(resources.getColor(R.color.edit_text_highlight_hint_color))
             }
 
