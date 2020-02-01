cc15c277edfe2f79377be1c4266101a65303f267
vbu_apptech <vbu@apptech-team.ru>
2020-02-01 16:31:04 +0300

    add icons for place information fragment

--- app/src/main/java/com/example/arenamsk/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/arenamsk/ui/map/MapFragment.kt
@@ -5,11 +5,17 @@ import android.view.View
 import androidx.lifecycle.ViewModelProviders
 import com.example.arenamsk.R
 import com.example.arenamsk.ui.base.BaseFragment
+import com.example.arenamsk.ui.places.PlacesViewModel
 
 class MapFragment : BaseFragment(R.layout.fragment_map) {
 
     private val mapViewModel by lazy {
         ViewModelProviders.of(this).get(MapViewModel::class.java)
+    }
+
+    //Нам также нужна PlacesViewModel для получения списка площадок
+    private val placeViewModel by lazy {
+        ViewModelProviders.of(this).get(PlacesViewModel::class.java)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
