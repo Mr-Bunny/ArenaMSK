2f004fdb8bb3d0f7c2afa06e39658134b126f970
vbu_apptech <vbu@apptech-team.ru>
2020-02-01 16:36:25 +0300

    update place model

--- app/src/main/java/com/example/arenamsk/models/PlaceModel.kt
+++ app/src/main/java/com/example/arenamsk/models/PlaceModel.kt
@@ -34,6 +34,30 @@ data class PlaceModel(
     var feedbackNumber: Int = 0,
 
     @SerializedName("imageUrl")
-    var imagesUrl: List<String> = emptyList()
+    var imagesUrl: List<String> = emptyList(),
+
+    @SerializedName("phone_numbers")
+    var phoneNumbersList: List<String> = emptyList(),
+
+    @SerializedName("total_area")
+    var totalArea: Int = 0,
+
+    @SerializedName("number_of_places")
+    var numberOfPlaces: Int = 0,
+
+    @SerializedName("has_parking")
+    var hasParking: Boolean = false,
+
+    @SerializedName("has_inventory")
+    var hasInventory: Boolean = false,
+
+    @SerializedName("open_field")
+    var openField: Boolean = false,
+
+    @SerializedName("has_lockers")
+    var hasLockers: Boolean = false,
+
+    @SerializedName("has_baths")
+    var hasBaths: Boolean = false
 
 ) : Parcelable
