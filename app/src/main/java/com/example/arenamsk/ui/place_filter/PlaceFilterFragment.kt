7c36a9fcfe38dda451fd553d5106007b90c09feb
vbu_apptech <vbu@apptech-team.ru>
2020-03-04 11:45:01 +0300

    fix price range bar and edittexts

--- app/src/main/java/com/example/arenamsk/ui/place_filter/PlaceFilterFragment.kt
+++ app/src/main/java/com/example/arenamsk/ui/place_filter/PlaceFilterFragment.kt
@@ -28,6 +28,9 @@ class PlaceFilterFragment private constructor() : DialogFragment(), LifecycleOwn
         }
     }
 
+    private var priceFrom = 0
+    private var priceTo = 100000
+
     private val placesViewModel by lazy {
         ViewModelProviders.of(this).get(PlacesViewModel::class.java)
     }
@@ -68,39 +71,65 @@ class PlaceFilterFragment private constructor() : DialogFragment(), LifecycleOwn
 
     //TODO на основе placeFilterModel выставляем UI
     private fun updateUI() {
-//        filter_start_price_edit_text.addTextChangedListener(object : TextWatcher {
-//            override fun afterTextChanged(p0: Editable?) {
-//            }
-//
-//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//            }
-//
-//            override fun onTextChanged(price: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                val priceFrom = if (price.isNullOrEmpty()) 0.0f else price.toString().toFloat()
-//
-//                filter_price_range_bar.setProgress(
-//                    priceFrom,
-//                    filter_end_price_edit_text.text.toString().toFloat()
-//                )
-//            }
-//        })
-//
-//        filter_end_price_edit_text.addTextChangedListener(object : TextWatcher {
-//            override fun afterTextChanged(p0: Editable?) {
-//            }
-//
-//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//            }
-//
-//            override fun onTextChanged(price: CharSequence?, p1: Int, p2: Int, p3: Int) {
-//                val priceTo = if (price.isNullOrEmpty()) 0.0f else price.toString().toFloat()
-//
-//                filter_price_range_bar.setProgress(
-//                    filter_start_price_edit_text.text.toString().toFloat(),
-//                    priceTo
-//                )
-//            }
-//        })
+        filter_start_price_edit_text.addTextChangedListener(object : TextWatcher {
+            var editing = false
+
+            override fun afterTextChanged(price: Editable?) {
+                priceFrom = if (price.isNullOrEmpty()) {
+                    0
+                } else {
+                    price.toString().toInt()
+                }
+
+                if (!editing) {
+                    editing = true
+                    filter_start_price_edit_text.setText(priceFrom.toString())
+                    filter_start_price_edit_text.setSelection(priceFrom.toString().length)
+                    editing = false
+                }
+
+                filter_price_range_bar.setProgress(
+                    priceFrom.toFloat(),
+                    priceTo.toFloat()
+                )
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+
+        filter_end_price_edit_text.addTextChangedListener(object : TextWatcher {
+            var editing = false
+
+            override fun afterTextChanged(price: Editable?) {
+                priceTo = if (price.isNullOrEmpty()) {
+                    1
+                } else {
+                    price.toString().toInt()
+                }
+
+                if (!editing) {
+                    editing = true
+                    filter_end_price_edit_text.setText(priceTo.toString())
+                    filter_end_price_edit_text.setSelection(priceTo.toString().length)
+                    editing = false
+                }
+
+                filter_price_range_bar.setProgress(
+                    priceFrom.toFloat(),
+                    priceTo.toFloat()
+                )
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
 
         filter_price_range_bar.setProgress(0f, 100000f)
 
@@ -113,16 +142,18 @@ class PlaceFilterFragment private constructor() : DialogFragment(), LifecycleOwn
             ) {
                 //Если число делится на тысячу без остатка, то выставляем его,
                 //иначе считаем ближайшее число, которое делится на 1000 без остатка
-                filter_start_price_edit_text.setText(
-                    if (leftValue.toInt() % 1000f != 0f) getValueFromSeekBar(
-                        leftValue.toInt()
-                    ).toString() else leftValue.toInt().toString()
-                )
-                filter_end_price_edit_text.setText(
-                    if (rightValue.toInt() % 1000f != 0f) getValueFromSeekBar(
-                        rightValue.toInt()
-                    ).toString() else rightValue.toInt().toString()
-                )
+                if (isFromUser) {
+                    filter_start_price_edit_text.setText(
+                        if (leftValue.toInt() % 1000f != 0f) getValueFromSeekBar(
+                            leftValue.toInt()
+                        ).toString() else leftValue.toInt().toString()
+                    )
+                    filter_end_price_edit_text.setText(
+                        if (rightValue.toInt() % 1000f != 0f) getValueFromSeekBar(
+                            rightValue.toInt()
+                        ).toString() else rightValue.toInt().toString()
+                    )
+                }
             }
 
             override fun onStartTrackingTouch(view: RangeSeekBar?, isLeft: Boolean) {
