e51399725e0dd9513c0ca76cd5ab522748f9d9fd
vbu_apptech <vbu@apptech-team.ru>
2020-04-19 14:09:56 +0300

    refactor orderviewholer

--- app/src/main/java/com/example/arenamsk/ui/booked/adapter/OrdersViewHolder.kt
+++ app/src/main/java/com/example/arenamsk/ui/booked/adapter/OrdersViewHolder.kt
@@ -1,10 +1,13 @@
 package com.example.arenamsk.ui.booked.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.arenamsk.R
 import com.example.arenamsk.models.OrderModel
 import com.example.arenamsk.models.PlaceModel
 import com.example.arenamsk.utils.TimeUtils
+import kotlinx.android.synthetic.main.current_booked_item.view.*
 import kotlinx.android.synthetic.main.item_current_order.view.*
 import java.lang.Exception
 
@@ -14,22 +17,38 @@ class OrdersViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         with(itemView) {
             itemView.setOnClickListener { itemClickCallback.invoke(order.place ?: PlaceModel(), -1) }
 
-            order_playground_text.text = order.playground?.sport?.name ?: "Вид спорта"
-            order_price_text.text = order.amount
-            order_date_text.text = TimeUtils.convertBookedDateAndTime(order.date, order.booking[0].from, order.booking[0].to)
-
-            val isHalfBooked = try {
-                order.booking[0].isHalfBooking
-            } catch (e: Exception) {
-                false
-            }
-
-            order_playground_title.text = if (isHalfBooked) "Половина площадки: " else "Площадка: "
-
             order.place?.let {
                 order_place_work_time_text.text = TimeUtils.convertWorkTime(it.workDayStartAt, it.workDayStartAt)
                 order_place_address_text.text = it.address
                 order_place_title.text = it.placeTitle
+            }
+
+            //Для каждого значения времени в order.booking создаем view и добавляем в общий контейнер
+            order.booking.forEachIndexed { index, bookingModel->
+                //Если мы бронировали несколько времен, то будем отображать цифры
+                val isNumberVisible = order.booking.size > 1
+
+                //Была ли забронирована половина или целая площадка
+                val isHalfBooked = try {
+                    bookingModel.isHalfBooking
+                } catch (e: Exception) {
+                    false
+                }
+
+                val bookedTimeView = LayoutInflater.from(itemView.context).inflate(R.layout.current_booked_item, booked_container, false).apply {
+                    this.order_playground_text.text = order.playground?.sport?.name ?: "Вид спорта"
+                    this.order_price_text.text = order.amount
+                    this.order_date_text.text = TimeUtils.convertBookedDateAndTime(order.date, order.booking[0].from, order.booking[0].to)
+
+                    this.order_playground_title.text = if (isHalfBooked) "Половина площадки: " else "Площадка: "
+
+                    this.order_number.visibility = if (isNumberVisible) View.VISIBLE else View.GONE
+                    val numberText = "${index + 1}) "
+                    this.order_number.text = numberText
+                }
+
+
+                booked_container.addView(bookedTimeView)
             }
         }
     }
